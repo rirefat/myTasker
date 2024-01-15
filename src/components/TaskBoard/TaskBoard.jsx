26b2d33408d88a797ea930a537ea8a9957be3448
rirefat <rirefat.official@gmail.com>
2024-01-15 23:15:38 +0600

    slightly modify the search box section

--- src/components/TaskBoard/TaskBoard.jsx
+++ src/components/TaskBoard/TaskBoard.jsx
@@ -8,14 +8,16 @@ const TaskBoard = () => {
 
             <div className="container">
                 {/* //<!-- Search Box --> */}
-                <SearchBox/>
+                <div className="p-2 flex justify-end">
+                    <SearchBox />
+                </div>
 
                 <div className="rounded-xl border border-[rgba(206,206,206,0.12)] bg-[#1D212B] px-6 py-8 md:px-9 md:py-16">
                     {/* Task Actions */}
-                    <TaskActions/>
+                    <TaskActions />
 
                     {/* Task Table */}
-                    <TaskTable/>
+                    <TaskTable />
                 </div>
             </div>
         </section>
